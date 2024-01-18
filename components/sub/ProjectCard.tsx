b16b0629103547a0187fe721880ad773ef08ac4e
Victor <victorcyty@hotmail.com>
2024-01-18 20:15:19 -0300

    ProjectCard image resized

--- components/sub/ProjectCard.tsx
+++ components/sub/ProjectCard.tsx
@@ -20,10 +20,9 @@ const ProjectCard: React.FC<ProjectCardProps> = ({
   return (
     <div className="rounded-lg overflow-hidden shadow-lg">
 <div
-  className="h-52 md:h-72 relative group"
+  className="h-52 md:h-65 relative group"
   style={{ background: `url(${imgUrl})`, backgroundSize: "100% 100%" }}
 >
-
         <div className="overlay items-center justify-center absolute top-0 left-0 w-full h-full bg-[#181818] bg-opacity-0 hidden group-hover:flex group-hover:bg-opacity-80 transition-all duration-500 ">
           <Link href={gitUrl} passHref className="h-14 w-14 mr-2 border-2 relative rounded-full border-[#ADB7BE] hover:border-[#020013] group/link">
               <CodeBracketIcon className="h-10 w-10 text-[#ADB7BE] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2  cursor-pointer group-hover/link:text-[#020013]" />
